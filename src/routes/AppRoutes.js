2421639ea742fcceaf1d9b6b8dec011dd8f2409e
Badri Vishal Singhal <badrivishalsinghal8803@gmail.com>
2023-04-02 20:10:38 +0530

    Temporary Changes

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,26 +1,19 @@
 import NavBar from "../components/NavBar";
-import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "../components/Home";
 import { AvailableHouses } from "../components/AvailableHouses";
-import { Login} from "../components/Login";
+import { Login } from "../components/Login";
 
 function AppRoutes() {
   return (
-    
-    <>
-     
-      <Router>
-        <NavBar />
-
-        <div className="pages">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/AvailableHouses" element={<AvailableHouses />} />
-            <Route path="/Login" element={<Login />} />
-          </Routes>
-        </div>
-      </Router>
-  </>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/AvailableHouses" element={<AvailableHouses />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
