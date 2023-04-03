48b5579d64ed801d325c67731a091e9b6a484f32
Badri Vishal Singhal <badrivishalsinghal8803@gmail.com>
2023-04-04 00:49:17 +0530

    Prediction Page Complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import "./css/home.css";
 export const Home = () => {
   return (
     <div className="homeContainer">
-      <h6 className="homePageHeading"></h6>
+      
       <h6 className="Predictprice">Show Available Houses</h6>
       <div className="imgContainer" >
         <img id="homePageImage" src={pic} alt="house" />
