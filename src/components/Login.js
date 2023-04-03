b3c984bfe327b15eca660d0bbf9e8d36d3523ce0
Badri Vishal Singhal <badrivishalsinghal8803@gmail.com>
2023-04-03 19:35:06 +0530

    Prediction Page Initialized

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,10 +19,10 @@ export const Login = () => {
         <div className="login-box">
           <h2>Login</h2>
           <form>
-            <label htmlFor="login-username">Username:</label>
+            <label className="margin-top" htmlFor="login-username">Username:</label>
             <input type="text" id="login-username" name="login-username" required />
 
-            <label htmlFor="login-password">Password:</label>
+            <label className="margin-top" htmlFor="login-password">Password:</label>
             <input type="password" id="login-password" name="login-password" required />
                
             <button type="submit">Login</button>
@@ -37,13 +37,13 @@ export const Login = () => {
         <div className="signup-box">
           <h2>Sign Up</h2>
           <form>
-            <label htmlFor="signup-name">Name:</label>
+            <label className="margin-top" htmlFor="signup-name">Name:</label>
             <input type="text" id="signup-name" name="signup-name" required />
 
-            <label htmlFor="signup-email">Email:</label>
+            <label className="margin-top" htmlFor="signup-email">Email:</label>
             <input type="email" id="signup-email" name="signup-email" required />
 
-            <label htmlFor="signup-password">Password:</label>
+            <label className="margin-top" htmlFor="signup-password">Password:</label>
             <input type="password" id="signup-password" name="signup-password" required />
 
             <button type="submit">Sign Up</button>
--- src/components/Predict.js
+++ src/components/Predict.js
@@ -1,10 +1,29 @@
 import React from "react";
-import "./css/Predict.css"
+import { PredictionForm } from "./PredictionForm";
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export const Predict = () => {
-  return (
-    <div className="PredictContainer">
-      <h6 className="Predict">Real Estate Price Predictor</h6>
-      </div>
-  );
+    return(
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } ,}}>
+          <Typography component="h1" variant="h4" align="center" sx={{ "marginBottom":"20px"}}>
+              <span className="predictiHeading">Real Estate Price Predictor</span>
+          </Typography>
+          <React.Fragment>
+              <PredictionForm />
+
+              <Button
+                  variant="contained"
+                  sx={{ mt: 3, ml: 1 }}
+                >
+                  {"Predict Price"}
+                </Button>
+            </React.Fragment>
+          </Paper>
+
+          </Container>
+    )
 };
