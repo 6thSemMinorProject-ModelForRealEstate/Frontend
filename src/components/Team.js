c9911ed8b4a6db0e7d57d9cf13f31b43eef7b5df
Badri Vishal Singhal <badrivishalsinghal8803@gmail.com>
2023-04-05 20:25:19 +0530

    Team Page Completed

--- src/components/Team.js
+++ src/components/Team.js
@@ -5,6 +5,7 @@ import pic from "../images/tejas.jpg";
 import pic2 from "../images/badri.jpg";
 import pic3 from "../images/shubh.jpg";
 import pic4 from "../images/nayan.jpg";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export const Team = () => {
 
@@ -19,6 +20,7 @@ export const Team = () => {
           <div class="col-md-12 text-center">
 
             <h2 class="section-title">The Team Behind Pricewise</h2>
+            <p className="teamText">The team behind PriceWise is dedicated to providing a valuable service to consumers by helping them make informed purchasing decisions and save money. Through their hard work and expertise, they have established PriceWise as a trusted and reliable price comparison platform that has helped countless people find great deals on products and services.</p>
           </div>
 
           <div class="col-sm-6 col-md-3">
@@ -27,9 +29,9 @@ export const Team = () => {
             <div class="team-item">
 
               <img id="tejasimg" src={pic} alt="tejas" class="team-img" />
-              <h3>Team leader</h3>
+              <h3>Frontend Developer</h3>
               <div class="team-info"><p>Tejas Jain</p></div>
-              <p>  To get in touch with me , click on the button below :- </p>
+              {/* <p>  To get in touch with me , click on the button below :- </p> */}
 
               <ul class="team-icon">
                 <li><a href="https://www.linkedin.com/in/tejas-jain-114288202/" class="linkedin">
@@ -45,9 +47,9 @@ export const Team = () => {
             <div class="team-item">
 
               <img id="badriimg" src={pic2} alt="badri" class="team-img" />
-              <h3>Team member</h3>
+              <h3>Fullstack and ML Developer</h3>
               <div class="team-info"><p>Badri vishal Singhal</p></div>
-              <p>To get in touch with me , click on the button below.</p>
+              {/* <p>To get in touch with me , click on the button below.</p> */}
 
               <ul class="team-icon">
 
@@ -64,9 +66,9 @@ export const Team = () => {
             <div class="team-item">
 
               <img id="shubhimg" src={pic3} alt="shubh" class="team-img" />
-              <h3>Team member </h3>
+              <h3>Frontend Developer </h3>
               <div class="team-info"><p>Shubh gupta</p></div>
-              <p>To get in touch with me , click on the button below</p>
+              {/* <p>To get in touch with me , click on the button below</p> */}
 
               <ul class="team-icon">
 
@@ -84,9 +86,9 @@ export const Team = () => {
             <div class="team-item">
 
             <img id="nayanimg" src={pic4} alt="nayan" class="team-img" />
-              <h3>Team member</h3>
+              <h3>Frontend Developer</h3>
               <div class="team-info"><p>Nayan gupta</p></div>
-              <p>To get in touch with me , click on the button below</p>
+              {/* <p>To get in touch with me , click on the button below</p> */}
 
               <ul class="team-icon">
 
