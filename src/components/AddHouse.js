04f7632c00c73e44675f392082278cb974041591
Badri Vishal Singhal <badrivishalsinghal8803@gmail.com>
2023-04-05 17:56:17 +0530

    Added Background image

--- src/components/AddHouse.js
+++ src/components/AddHouse.js
@@ -257,11 +257,11 @@ export const AddHouse = () => {
   };
 
   return (
-
+    <div className="bg1">
     <Container component="main" maxWidth="sm" sx={{ mb: 4 }} >
       <Paper
         variant="outlined"
-        sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+        sx={{ my: { xs: 0, md: 0 }, p: { xs: 2, md: 3 } }}
       >
         <Typography
           component="h1"
@@ -609,6 +609,7 @@ export const AddHouse = () => {
       {/* Same as */}
       <ToastContainer />
     </Container>
+    </div>
 
   );
 };
