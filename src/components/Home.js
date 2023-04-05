a5b09ec51784dbc6a4e60e3175a5cfa84f2c4808
Badri Vishal Singhal <badrivishalsinghal8803@gmail.com>
2023-04-05 17:58:39 +0530

    Initialised homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,81 @@
 import React from "react";
 import pic from "../images/house.png";
+import pic1 from "../images/buyHome.webp";
+import pic2 from "../images/sellHome.webp";
+import pic3 from "../images/predictHome.webp";
+
 import "./css/home.css";
 
 export const Home = () => {
   return (
-    <div className="homeContainer">
-      
-      <h6 className="Predictprice">Show Available Houses</h6>
-      <div className="imgContainer" >
-        <img id="homePageImage" src={pic} alt="house" />
+    <>
+    <div class="cards">
+        <div class="card">
+          <div class="card__image-holder">
+            <img class="card__image" src={pic1} alt="wave" />
+          </div>
+          <div class="card-title">
+            <h2>Buy a home</h2>
+          </div>
+          <div>
+            <div class="card-description">
+              Find your place with an immersive photo experience and the most
+              listings, including things you won’t find anywhere else.
+            </div>
+            <div>
+              <div class="card-actions">
+                <a href="#" class="btn">
+                  Browse homes
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card__image-holder">
+            <img class="card__image" src={pic2} alt="beach" />
+          </div>
+          <div class="card-title">
+            <h2>Sell a home</h2>
+          </div>
+          <div>
+            <div class="card-description">
+            From preparing your home for showings to negotiating the best price, we're here to guide you every step of the way.
+            </div>
+            <div>
+              <div class="card-actions">
+                <a href="#" class="btn">
+                  See your options
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card__image-holder">
+            <img class="card__image" src={pic3} alt="mountain" />
+          </div>
+          <div class="card-title">
+            <h2>Predict price of home</h2>
+          </div>
+          <div>
+            <div class="card-description">
+              We’re creating a seamless online experience – from shopping on the
+              largest rental network, to applying, to predictiong price of a house.
+            </div>
+            <div>
+              <div class="card-actions">
+                <a href="#" class="btn">
+                  Predict Price
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      
+    </>
   );
 };
