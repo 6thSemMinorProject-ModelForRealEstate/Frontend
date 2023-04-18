a2178d80d75fefa5c8cd920d7b46dc6885b208a3
Badri Vishal Singhal <badrivishalsinghal8803@gmail.com>
2023-04-18 22:18:47 +0530

    Available House Added

--- src/components/AvailableHouse.js
+++ src/components/AvailableHouse.js
@@ -1,9 +1,39 @@
 import React from "react";
 import pic5 from "../images/Most-Beautiful-House-in-the-World.jpg";
 import { Margin } from "@mui/icons-material";
+import { useState, useEffect } from "react";
+import Axios from "../config/Axios"
 
 export const AvailableHouse = () => {
-    var temp = [1,2,3,4,5,6,7,8,9,10]
+    var temp = [1,2,3,4,5,6,7,8,9,10];
+    const [houses, setHouses] = useState([]);
+
+    const getHouses = async()=> {
+        const config = {
+            headers: {
+                "Content-type": "application/json",
+            },
+        };
+        
+        const result = await Axios.get("/house/availableHouses", config);
+        console.log(result.data.houses);
+        let temp = result.data.houses;
+        for(let i = 0; i < temp.length; i++) {
+            console.log(temp[i])
+            const email = await  Axios.post("/auth/getEmailId", {id:temp[i].userId} , config);
+            console.log(email.data.emailId)
+            temp[i]['emailId'] = (email.data.emailId)
+        }
+        console.log(temp)
+        setHouses(temp);
+    } 
+
+    useEffect(()=>{
+        getHouses();
+    }, []);
+
+    
+
     return (
         <div>
         <div>
@@ -12,7 +42,7 @@ export const AvailableHouse = () => {
           <div class="cards">
         
         {
-            temp.map((item, index)=>{
+            houses.map((item, index)=>{
                 return (<div class="card">
                     <div class="card__image-holder">
                         <img class="card__image" src={pic5} alt="beach" />
@@ -22,16 +52,16 @@ export const AvailableHouse = () => {
                     </div>
                     <div>
                         <div class="card-description">
-                        Area:
+                        Area: {item.area}
                         </div>
                         <div class="card-description">
-                        Location:
+                        Location: {item.location}
                         </div>
                         <div class="card-description">
-                        City:
+                        City: {item.city}
                         </div>
                         <div class="card-description">
-                        Contact Email:
+                        Contact Email: {item.emailId}
                         </div>
                     </div>
                 </div>
